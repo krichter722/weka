0cc31855b9d16cc2f4cc91f3298cd9440823a61b
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2015-03-25 20:52:02 +0000

    Javadoc output is now Java 8 compliant (HTML 4 does not allow empty tags like <br/> or <p/>)

--- weka/src/main/java/weka/core/TechnicalInformationHandlerJavadoc.java
+++ weka/src/main/java/weka/core/TechnicalInformationHandlerJavadoc.java
@@ -15,7 +15,7 @@
 
 /*
  * TechnicalInformationHandlerJavadoc.java
- * Copyright (C) 2006-2012 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2006-2012,2015 University of Waikato, Hamilton, New Zealand
  */
 
 package weka.core;
@@ -27,10 +27,10 @@ import java.util.Vector;
 /**
  * Generates Javadoc comments from the TechnicalInformationHandler's data.
  * Update the BibTex references and the plaintext techincal information.
- * <p/>
+ * <p>
  * 
  * <!-- options-start --> Valid options are:
- * <p/>
+ * <p>
  * 
  * <pre>
  * -W &lt;classname&gt;
@@ -209,8 +209,8 @@ public class TechnicalInformationHandlerJavadoc extends Javadoc {
       }
       result += "<pre>\n";
       result += toHTML(handler.getTechnicalInformation().toBibTex())
-        .replaceAll("<br/>", "") + "\n";
-      result += "</pre>\n<p/>\n";
+        .replaceAll("<br>", "") + "\n";
+      result += "</pre>\n<p>\n";
       break;
     }
 
